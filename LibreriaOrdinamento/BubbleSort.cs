c95bf72ba48ea17d2be422d8246f624855af8f8c
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-02-19 13:26:32 +0100

    Add project files.

---
+++ LibreriaOrdinamento/BubbleSort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetodiOrdinamenti
+{
+    public class BubbleSort
+    {
+        public static int[] Bubble_Sort(int[] a)
+        {
+            for (int x = 0; x < a.Length - 1; x++)
+            {
+                for (int y = 0; y < a.Length - 1; y++)
+                {
+                    if (a[y] > a[y + 1])
+                    {
+                        int tempo = a[y];
+                        a[y] = a[y + 1];
+                        a[y + 1] = tempo;
+                    }
+                }
+            }
+            return a;
+        }
+    }
+}
---
+++ Ordinamento/Program.cs
@@ -0,0 +1,34 @@
+﻿using MetodiOrdinamenti;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace Ordinamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensioni = { 10, 20, 50, 100, 500, 1000, 2000, 5000, 10000, 20000, 30000, 40000, 50000 };
+            using (StreamWriter w = new StreamWriter("stat.csv", false, Encoding.UTF8))
+            {
+                w.WriteLine("Algoritmo ; Dimensione ; Tempo");
+                foreach (int dim in dimensioni)
+                {
+                    // 1. Crea array
+                    int[] numeri = new int[dim];
+                    // 2. Genera casuali 
+                    Random r = new Random();
+                    for (int i = 0; i < dim; i++)
+                        numeri[i] = r.Next();
+                    // 3. Parte cronometro
+                    Stopwatch s = new Stopwatch();
+                    // 4. Ordina
+                    BubbleSort.Bubble_Sort(numeri);
+                    InsertSort
+                }
+            }
+        }
+    }
+}
